f96f868b6788cd7214d3268dcaddcee0e817acf8
Jonathan <jousca7@hotmail.com>
2020-01-15 21:47:29 -0500

    Removed unnecessary functions

--- server.py
+++ server.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, redirect
 import csv
 
 app = Flask(__name__)
-
-
 # print(__name__)
 
 
@@ -49,34 +47,5 @@ def submit_form():
         return "Something went wrong. Try again"
 
 
-"""@app.route("/index.html")
-def back_home():
-    return render_template("index.html")
-
-
-@app.route("/works.html")
-def my_works():
-    return render_template("/works.html")
-
-
-@app.route("/work.html")
-def work():
-    return render_template("/work.html")
-
-
-@app.route("/about.html")
-def about():
-    return render_template("about.html")
-
-
-@app.route("/contact.html")
-def contact_me():
-    return render_template("contact.html")
-
-
-@app.route("/components.html")
-def components():
-    return render_template("components.html")"""
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True)git
